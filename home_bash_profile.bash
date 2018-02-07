27de7dd7eca42ae35bbc370ec481eda706f559a1
themoffatt <adam@themoffatt.com>
2018-02-07 12:16:17 -0600

    updated dot files

--- home_bash_profile.bash
+++ home_bash_profile.bash
@@ -8,5 +8,14 @@ parse_git_branch() {
 
 # Put the git branch in the prompt
 export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}\007"'
+export EDITOR=vim
 
-alias up="cd .."
+alias resetpacketfilter='sudo pfctl -f /etc/pf.conf; sudo pfctl -e'
+alias flushdns='sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;'
+alias grep='grep --color=auto'
+alias be='bundle exec'
+alias rake='be rake'
+alias rspec='be rspec'
+alias up='cd ..'
+alias go=go_to_project
